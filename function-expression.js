d3bf837a98fea0b6011e481be6bfeac1f8bd7585
Mr-Hanu <1200xxAb7MLOL@gmx.com>
2017-03-11 10:37:38 +0100

    Add files via upload

---
+++ function-expression.js
@@ -0,0 +1,5 @@
+var area = function(width, height) {					// anonymous function, stored in a variable
+	return width * height;								// what's returned to the caller
+}
+
+var size = area(3, 4);									// function gets called and stored in another variable 
